c668fe7af15f979e6bb72b62227157d16ca5b550
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-03 22:13:19 +0530

    type conversion

---
+++ projects/typeConversion/index.js
@@ -0,0 +1,11 @@
+//Website header and Title text
+
+let webtitle = document.getElementById("webtiele")
+let title = document.getElementById("title");
+const titleString="Type Conversion Application"
+title.innerText=titleString;
+webtitle.innerText=titleString;
+console.log(titleString);
+
+
+
