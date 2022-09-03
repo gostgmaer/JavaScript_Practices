a9935e37b8691f91cfa93461a3baaeb736605e05
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-03 22:09:04 +0530

    new code for input data and show it

--- projects/inoutdata/index.js
+++ projects/inoutdata/index.js
@@ -2,10 +2,24 @@
 
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
-const titleString="Demo Web App"
+const titleString="User Input data"
 title.innerText=titleString;
 webtitle.innerText=titleString;
 console.log(titleString);
 
 
+//programming
 
+let user ;
+let btn =  document.getElementById("button");
+let userfield= document.getElementById("nametxt");
+let rightSection = document.getElementById("right-section");
+
+btn.addEventListener("click",function(){
+   
+    user = userfield.value;
+    let newlabel = document.createElement("label");
+    newlabel.innerText=user;
+    rightSection.append(newlabel.innerText=user);
+    console.log("btn Working",user);
+})
