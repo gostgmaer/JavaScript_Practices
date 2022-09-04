351f5c18b77a4a8bef72e8b4fe59df2288c3170c
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 11:27:02 +0530

    conditions

--- projects/Checked_Property/index.js
+++ projects/Checked_Property/index.js
@@ -8,4 +8,23 @@ webtitle.innerText=titleString;
 console.log(titleString);
 
 
+let btn = document.getElementById("button");
 
+
+btn.addEventListener("click",function (params) {
+    let check =document.getElementById("checkbox");
+    let visa =document.getElementById("visa");
+    let mastercard =document.getElementById("mastercard");
+    let paypal =document.getElementById("paypal");
+    if (check.checked) {
+        console.log("You are subcribed");
+        
+    } else {
+        console.log("You are Not subcribed");
+        
+    }
+    console.log(check.value);
+
+    
+    
+})
