76aa5aae0de6fc7916b9a220abe8bdfaa9aa44b5
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 10:53:12 +0530

    Checked Property

---
+++ projects/Checked_Property/index.js
@@ -0,0 +1,11 @@
+//Website header and Title text
+
+let webtitle = document.getElementById("webtiele")
+let title = document.getElementById("title");
+const titleString="Checked Property Web App"
+title.innerText=titleString;
+webtitle.innerText=titleString;
+console.log(titleString);
+
+
+
