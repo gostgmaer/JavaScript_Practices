9e48f65f1fd625bcb0215bac2ae8788c60c23b06
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 12:51:09 +0530

    Currency Conversion

--- projects/Functions/index.js
+++ projects/Functions/index.js
@@ -8,4 +8,15 @@ webtitle.innerText=titleString;
 console.log(titleString);
 
 
+//Convert to the Local String Language
 
+let myNum = 123456.789;
+// myNum = myNum.toLocaleString("en-US");
+// myNum = myNum.toLocaleString("hi-IN");
+myNum = myNum.toLocaleString("en-uk",{style: "currency",currency:"CAD"});
+console.log(myNum);
+function firstFunction(params) {
+    console.log("firstFunction Working");
+}
+
+firstFunction(myNum);
