2d45fbe95710a4dded8be1fee83e35c77d67e48a
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 14:39:39 +0530

    toLocaleString

--- projects/Functions/index.js
+++ projects/Functions/index.js
@@ -2,18 +2,21 @@
 
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
-const titleString="Functions Web App"
-title.innerText=titleString; 
-webtitle.innerText=titleString;
+const titleString = "Functions Web App"
+title.innerText = titleString;
+webtitle.innerText = titleString;
 console.log(titleString);
 
 
 //Convert to the Local String Language
 
-let myNum = 123456.789;
+let myNum = 22;
 // myNum = myNum.toLocaleString("en-US");
 // myNum = myNum.toLocaleString("hi-IN");
-myNum = myNum.toLocaleString("en-uk",{style: "currency",currency:"CAD"});
+// myNum = myNum.toLocaleString("en-uk",{style: "currency",currency:"CAD"});
+// myNum = myNum.toLocaleString("undefine", { style: "percent" });
+myNum = myNum.toLocaleString(undefined, { style: "unit", unit: "Fahrenheit" });
+
 console.log(myNum);
 function firstFunction(params) {
     console.log("firstFunction Working");
