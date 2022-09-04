15b9b1aa2c90d78d185548e3246d5522e124e89f
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 14:54:36 +0530

    numberGuessingGame Done

--- projects/numberGuessingGame/index.js
+++ projects/numberGuessingGame/index.js
@@ -2,10 +2,33 @@
 
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
-const titleString="Demo Web App"
+const titleString="numberGuessingGame Web App"
 title.innerText=titleString;
 webtitle.innerText=titleString;
 console.log(titleString);
 
 
+//Apps
 
+const ans= Math.floor(Math.random()*10+1);
+let guessss = 0;
+
+let btn = document.getElementById("button")
+
+btn.addEventListener("click",function (params) {
+    let guess = document.getElementById("enternumber").value;
+    guessss +=1;
+    if (guess=ans) {
+        alert(`${ans} is the #number. It's too you ${guessss} guesses`)
+        console.log(`${ans} is the #number. It's too you ${guessss} guesses`);
+        
+    } else if(guess<ans) {
+        alert("Too Small")
+        console.log("Too Small");
+        
+    }else{
+        alert("Too Big")
+        console.log("Too Big");
+    }
+
+})
