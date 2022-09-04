8813e0393f1693a0ae09ef547726ce9dfaecdc6b
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 14:40:41 +0530

    numberGuessingGame

---
+++ projects/numberGuessingGame/index.js
@@ -0,0 +1,11 @@
+//Website header and Title text
+
+let webtitle = document.getElementById("webtiele")
+let title = document.getElementById("title");
+const titleString="Demo Web App"
+title.innerText=titleString;
+webtitle.innerText=titleString;
+console.log(titleString);
+
+
+
