5d1938a295f5c1f26bb7957351aee1b1e641e8bf
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 19:02:44 +0530

    application Modal Updated

--- projects/TempratureConversion/index.js
+++ projects/TempratureConversion/index.js
@@ -3,10 +3,10 @@
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
 let modalTitle = document.getElementById("modal-title");
-const titleString="numberGuessingGame Web App"
-title.innerText=titleString;
-webtitle.innerText=titleString;
-modalTitle.innerText=titleString;
+const titleString = "TempratureConversion Web App"
+title.innerText = titleString;
+webtitle.innerText = titleString;
+modalTitle.innerText = titleString;
 console.log(titleString);
 
 //Show parent Div as a Modal
@@ -15,6 +15,9 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btns = document.getElementById("myBtn");
+btns.onclick = function () {
+  modal.style.display = "block";
+}
 var btnsss = document.querySelector("#body-class");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -24,18 +27,27 @@ var span = document.getElementsByClassName("close")[0];
 //   modal.style.display = "block";
 // }
 
-window.onload =  function() {
-    modal.style.display = "block";
-  }
+window.onload = function () {
+  modal.style.display = "block";
+}
+
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+
+
+
+
+
+
 
