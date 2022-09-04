725523d28957a7638e08fda7a870605c381eff0d
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 19:50:19 +0530

    ArracyPractices Web App

---
+++ projects/ArracyPractices/index.js
@@ -0,0 +1,53 @@
+//Website header and Title text
+
+let webtitle = document.getElementById("webtiele")
+let title = document.getElementById("title");
+let modalTitle = document.getElementById("modal-title");
+const titleString = "ArracyPractices Web App"
+title.innerText = titleString;
+webtitle.innerText = titleString;
+modalTitle.innerText = titleString;
+console.log(titleString);
+
+//Show parent Div as a Modal
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btns = document.getElementById("myBtn");
+btns.onclick = function () {
+  modal.style.display = "block";
+}
+var btnsss = document.querySelector("#body-class");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// btns.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+window.onload = function () {
+  modal.style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
