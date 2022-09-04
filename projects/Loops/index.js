364d2ac7ad38d193702cecbcc7f6a4675a9cf5c1
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 12:12:45 +0530

    tarnary Condition

--- projects/Loops/index.js
+++ projects/Loops/index.js
@@ -2,12 +2,20 @@
 
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
-const titleString="Loop Web App"
-title.innerText=titleString;
-webtitle.innerText=titleString;
+const titleString = "Loop Web App"
+title.innerText = titleString;
+webtitle.innerText = titleString;
 console.log(titleString);
 
 
 ///
 
 
+//c0ndition
+
+let adalt = checkAge(112);
+console.log(adalt);
+function checkAge(age) {
+    return age >= 18 ? true : false;
+
+}
