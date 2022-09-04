ffd5f7135140cbb7a16dc99f720611c9681e2efb
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 09:38:40 +0530

    HypotenuseCalc add

--- projects/hypotenuseCalc/index.js
+++ projects/hypotenuseCalc/index.js
@@ -2,10 +2,23 @@
 
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
-const titleString="Demo Web App"
+const titleString="HypotenuseCalc Web App"
 title.innerText=titleString;
 webtitle.innerText=titleString;
 console.log(titleString);
+
+
+
+let a,b,c;
+a= window.prompt("Enter Side A");
+a=Number(a)
+b=window.prompt("Enter Side B");
+b=Number(b);
+
+c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+
+console.log("Side C",c);
+
 
 
 
