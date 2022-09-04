4cf3f6e19a37e290c1999f1c1886b4399925e9ec
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 14:14:06 +0530

    unctions Web App

--- projects/Functions/index.js
+++ projects/Functions/index.js
@@ -3,7 +3,7 @@
 let webtitle = document.getElementById("webtiele")
 let title = document.getElementById("title");
 const titleString="Functions Web App"
-title.innerText=titleString;
+title.innerText=titleString; 
 webtitle.innerText=titleString;
 console.log(titleString);
 
