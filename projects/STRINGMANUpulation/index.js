25c6993dbe0b912670a57972ff95bf7b55885ec1
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-09-04 10:43:15 +0530

    method

--- projects/STRINGMANUpulation/index.js
+++ projects/STRINGMANUpulation/index.js
@@ -7,5 +7,22 @@ title.innerText=titleString;
 webtitle.innerText=titleString;
 console.log(titleString);
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+let userName = "                              Kishor Sarkar                "
+console.log(userName.length);
+console.log(userName.charAt(0));
+
+console.log(userName.indexOf("h"));
+
+console.log(userName.lastIndexOf("r"));
+userName= userName.trim();
+
+console.log(userName);
+
+userName= userName.replace("s","r")
+
+console.log(userName);
+
 
 
